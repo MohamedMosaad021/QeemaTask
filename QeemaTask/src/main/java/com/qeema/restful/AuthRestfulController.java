c7e84fb4296f68af0cbcaa4c4ac41d00c7ca7b36
moham <moham@Mohamed>
2022-10-30 23:26:14 +0200

    .

--- QeemaTask/src/main/java/com/qeema/restful/AuthRestfulController.java
+++ QeemaTask/src/main/java/com/qeema/restful/AuthRestfulController.java
@@ -35,7 +35,7 @@ import com.qeema.service.UserService;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
-public class AuthController {
+public class AuthRestfulController {
 	
 	@Autowired
 	AuthenticationManager authenticationManager;
